29bb9e21db2e3ee028280c2266b5005d5547bd2c
mlcoding111 <kennycodes@hotmail.com>
2022-03-21 20:31:47 -0400

    Added styling file for each component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 import Home from './Home/Home.jsx'
-import Header from './Header/Header.jsx'
-import Navbar from './Navbar/Navbar.jsx'
-import Footer from "./Footer/Footer.jsx";
+
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
       <Home/>
-      <Footer/>
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
 import Header from '../Header/Header.jsx'
+import Navbar from '../Navbar/Navbar.jsx'
+import Footer from "../Footer/Footer.jsx";
 
 const Home = () => {
   return (
     <>
+      <Navbar/>
       <Header/>
+
+        <main>
+          
+        </main>
+        
+      <Footer/>
     </>
   )
 }
