b29c82cb92715cd9ce7381d634ad8b05be60ab9e
mlcoding111 <kennycodes@hotmail.com>
2022-06-02 21:49:27 -0400

    Remplaced flex layout for grid

--- src/components/Projects/ProjectsCardsList.jsx
+++ src/components/Projects/ProjectsCardsList.jsx
@@ -6,7 +6,7 @@ import {data} from './data.js'
 export default function ProjectCardList() {
 
   return (
-    <div className="container">
+    <div>
       <div className="projects-list">
         {data.map((item, i) => (
             <Work item={item} title={item.title} src={item.src} link={item.website} gitlink={item.github} technologies={item.technologies} icons={item.icons} key={i}/>
